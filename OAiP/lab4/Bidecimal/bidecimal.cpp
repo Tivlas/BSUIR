1fb54da0a602c32c2388e6d9a2b8440385371d21
Tivlas <tima021003tima@gmail.com>
2022-04-26 12:54:49 +0300

    Сделал двудесятичные

--- OAiP/lab4/Bidecimal/bidecimal.cpp
+++ OAiP/lab4/Bidecimal/bidecimal.cpp
@@ -1,8 +1,111 @@
-#include "bidecimal.h"
+﻿#include "bidecimal.h"
 #include "stdafx.h"
 
-Bidecimal::Bidecimal(QWidget *parent)
-    : QMainWindow(parent)
+Bidecimal::Bidecimal(QWidget* parent)
+	: QMainWindow(parent)
 {
-    ui.setupUi(this);
+	ui.setupUi(this);
+	this->setFixedHeight(139);
+	this->setFixedWidth(669);
+}
+
+bool Bidecimal::k_bit(std::string str, int k)
+{
+	int bit, i = 0;
+	int lenstr = str.size();
+
+	for (; i != k; i++)
+	{
+		bit = (str[lenstr - 1] - '0') % 2;
+		str[lenstr - 1] = str[lenstr - 1] - bit;
+
+		for (int j = lenstr - 1; j >= 0; j--)
+		{
+			if ((str[j] - '0') % 2 == 0)
+			{
+				str[j] = (str[j] - '0') / 2 + '0';
+			}
+			else
+			{
+				str[j] = (str[j] - '0') / 2 + '0';
+
+				if (j != lenstr - 1)
+				{
+					str[j + 1] = str[j + 1] + 5;
+				}
+
+			}
+		}
+	}
+
+	return bit;
+}
+
+std::string Bidecimal::powstr(const std::string& str, int k)
+{
+	std::string deg = "1";
+
+	if (k == 0)
+	{
+		return("1");
+	}
+
+	for (int i = 0; i < k; i++)
+	{
+		deg += '0';
+	}
+
+	for (int i = deg.size() - 1, j = str.size() - 1; i >= 0 && j >= 0; i--, j--)
+	{
+		deg[i] = str[j];
+	}
+
+	return deg;
+}
+
+void Bidecimal::on_calcBtn_clicked() {
+	A[0] = "0";
+
+	int counterB, counterA = 1, k = 1, counterBIN = 1, tmp;
+
+	int n = ui.numberEdit->text().toInt();
+	if (n < 1 || n>10000) {
+		QMessageBox::warning(this, "Внимание", "Число должно принадлежать промежутку [1,10000].\nВведите число заново");
+		ui.numberEdit->clear();
+		return;
+	}
+
+
+	while (n >= counterBIN)
+	{
+		tmp = counterB = 0;
+
+		for (int i = 0; i < counterA; i++)
+		{
+			if (!k_bit(A[i], k))
+			{
+				A[tmp] = A[i];
+				tmp++;
+			}
+		}
+
+		counterA = tmp;
+
+		for (int i = 0; i < counterA; i++)
+		{
+			B[i] = powstr(A[i], k - 1);
+			counterB++;
+		}
+
+		for (int i = 0; i < counterB; i++)
+		{
+			A[counterA] = B[i];
+			counterA++;
+		}
+
+		counterBIN += counterB;
+		k++;
+	}
+	ui.numberEdit->clear();
+	ui.plainTextEdit->setPlainText(QString(B[counterB - (counterBIN - n)].c_str()));
 }
--- OAiP/lab4/Bidecimal/bidecimal.h
+++ OAiP/lab4/Bidecimal/bidecimal.h
@@ -11,8 +11,14 @@ class Bidecimal : public QMainWindow
 
 public:
     Bidecimal(QWidget *parent = Q_NULLPTR);
+    bool k_bit(std::string str, int k);
+    std::string powstr(const std::string& str, int k);
+public slots:
+    void on_calcBtn_clicked();
 
 private:
     Ui::BidecimalClass ui;
     Vector<std::string> A = std::move(Vector<std::string>(10000));
+    Vector<std::string> B = std::move(Vector<std::string>(10000));
+    bool clicked = false;
 };
