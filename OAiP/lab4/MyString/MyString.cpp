21b54ff111af1291502e62907aabb9bb34804086
Tivlas <tima021003tima@gmail.com>
2022-04-27 20:01:42 +0300

    Осталось реализовать еще несколько методов

--- OAiP/lab4/MyString/MyString.cpp
+++ OAiP/lab4/MyString/MyString.cpp
@@ -1,11 +1,14 @@
 #include "pch.h"
 #include "MyString.h"
-#include <exception>
-#include <cstring>
+
+MyString::MyString()
+{
+}
 
 MyString::MyString(size_t size) :str(new char[size + 1]), sz(size)
 {
 	str[sz] = '\0';
+	cap = sz;
 }
 
 MyString::MyString(size_t size, char ch) : str(new char[size + 1]), sz(size)
@@ -15,6 +18,7 @@ MyString::MyString(size_t size, char ch) : str(new char[size + 1]), sz(size)
 		str[i] = ch;
 	}
 	str[sz] = '\0';
+	cap = sz;
 }
 
 MyString::MyString(const MyString& other)
@@ -26,205 +30,130 @@ MyString::MyString(const MyString& other)
 		str[i] = other.str[i];
 	}
 	str[sz] = '\0';
+	cap = other.cap;
 }
 
 MyString::MyString(const char other[])
 {
-	size_t sz = std::strlen(other);
+	sz = my_strlen(other);
 	this->str = new char[sz + 1];
 	memcpy(this->str, other, sz + 1);
+	cap = sz;
 }
-
-
 
 MyString::~MyString()
 {
 	delete[] str;
 }
 
-void* MyString::memcpy(void* dest, const void* src, size_t n)
+size_t MyString::size() const
 {
-	char* p1 = (char*)dest;
-	const char* p2 = (const char*)src;
-	while (n--) {
-		*p1++ = *p2++;
-	}
-	return p1;
+	return sz;
 }
 
-void* MyString::memmove(void* dest, const void* src, size_t n)
+size_t MyString::capacity() const
 {
-
-	char* p1 = (char*)dest;
-	const char* p2 = (const char*)src;
-	while (n--) {
-		*p1++ = *p2++;
-	}
-	return p1;
+	return cap;
 }
 
-char* MyString::strcpy(char* dest, const char* src)
+void MyString::reserve(size_t new_cap)
 {
-	char* p1 = dest;
-	const char* p2 = src;
-	while (*p2) {
-		if (*p1 == '\0' && *p2 != '\0') {
-			throw std::exception("strcpy: dest is too small");
+	if (new_cap <= cap) return;
+
+	char* new_str = new char[new_cap];
+	for (size_t i = 0; i < sz; i++) {
+		new_str[i] = str[i];
+	}
+	delete[] str;
+	str = new_str;
+	cap = new_cap;
+}
+
+void MyString::resize(size_t new_size, const char ch = '\0')
+{
+	if (new_size > cap) reserve(new_size);
+	if (new_size > sz) {
+		for (size_t i = sz; i < new_size; i++) {
+			str[i] = ch;
 		}
-		*p1++ = *p2++;
 	}
-	*p1 = '\0';
-	return p1;
+	sz = new_size;
 }
 
-char* MyString::strncpy(char* dest, const char* src, size_t n)
+char& MyString::operator[](size_t pos)
 {
-	size_t already_copied = 0;
-	char* p1 = dest;
-	const char* p2 = src;
-	while (n-- && *p2) {
-		if (*p1 == '\0' && already_copied < n)
-		{
-			throw std::exception("strncpy: dest is too small");
-		}
-		*p1++ = *p2++;
-		++already_copied;
+	return str[pos];
+}
+
+char& MyString::at(size_t pos)
+{
+	if (pos >= sz)
+		throw std::out_of_range("Out of range");
+	return str[pos];
+}
+
+void MyString::push_back(char ch)
+{
+	if (sz == cap) {
+		reserve(cap * 2);
 	}
-	if (!*p2) {
-		*p1 = '\0';
-		++already_copied;
+	str[sz] = ch;
+	sz++;
+	str[sz] = '\0';
+}
+
+void MyString::pop_back()
+{
+	if (sz == 0) {
+		throw std::exception("Pop back empty string");
 	}
-	else if (already_copied != n) // if the source string is shorter than n
+	--sz;
+	str[sz] = '\0';
+}
+
+void MyString::clear()
+{
+	sz = 0;
+	str[sz] = '\0';
+}
+
+MyString& MyString::append(size_t count, char ch)
+{
+	if (cap - sz < count)
 	{
-		while (already_copied < n) {
-			*p1++ = '\0';
-			++already_copied;
-		}
+		reserve(sz + count);
 	}
-	return p1;
+	for (size_t i = 0; i < count; i++) {
+		str[sz + i] = ch;
+	}
+	sz += count;
+	str[sz] = '\0';
+	return *this;
 }
 
-char* MyString::strcat(char* dest, const char* src)
+char& MyString::front()
 {
-	char* p1 = dest;
-	while (*p1) {
-		p1++;
-	}
-	const char* p2 = src;
-	while (*p2) {
-		*p1++ = *p2++;
-	}
-	*p1 = '\0';
-	return p1;
+	return str[0];
 }
 
-char* MyString::strncat(char* dest, const char* src, size_t n)
+char& MyString::back()
 {
-	char* p1 = dest;
-	while (*p1) {
-		p1++;
-	}
-	const char* p2 = src;
-	while (n-- && *p2) {
-		*p1++ = *p2++;
-	}
-	*p1 = '\0';
-	return p1;
+	return str[sz - 1];
 }
 
-int MyString::memcmp(const void* s1, const void* s2, size_t n)
+const char* MyString::data() const
 {
-	const char* p1 = (const char*)s1;
-	const char* p2 = (const char*)s2;
-	while (n--) {
-		if (*p1 != *p2) {
-			return *p1 - *p2;
-		}
-		p1++;
-		p2++;
-	}
-	return 0;
-}
-
-int MyString::strcmp(const char* s1, const char* s2)
-{
-	while (*s1 && *s2) {
-		if (*s1 != *s2) {
-			return *s1 - *s2;
-		}
-		s1++;
-		s2++;
-	}
-	return *s1 - *s2;
-}
-
-int MyString::strcoll(const char* s1, const char* s2)
-{
-	return std::strcoll(s1, s2);
-}
-
-int MyString::strncmp(const char* s1, const char* s2, size_t n)
-{
-	while (n-- && *s1 && *s2) {
-		if (*s1 != *s2) {
-			return *s1 - *s2;
-		}
-		s1++;
-		s2++;
-	}
-	return *s1 - *s2;
-}
-
-size_t MyString::strxfrm(char* s1, const char* s2, size_t n)
-{
-	return std::strxfrm(s1, s2, n);
-}
-
-bool MyString::findChar(const char* str, char ch)
-{
-	while (*str) {
-		if (*str == ch) {
-			return true;
-		}
-		str++;
-	}
-	return false;
-}
-
-char* MyString::strtok(char* str, const char* delim)
-{
-	//static char* storage;
-	char check1 = '\0';
-	char first = *str;
-	char* first_address = nullptr;
-	char second;
-	while (*str) {
-		first_address = str;
-		first = *str;
-		if (*str && !findChar(delim, first)) {
-			check1 = 'c';
-			break;
-		}
-		str++;
-	}
-	if (check1 == '\0') {
-		return nullptr;
-	}
-	char check2 = '\0';
-	while (*(++first_address)) {
-		second = *first_address;
-		if (*first_address && findChar(delim, second)) {
-			second = '\0';
-			check2 = 'c';
-			break;
-		}
-		first_address++;
-	}
-	if (check2 != '\0') {
-		*(str) = '\0';
-	}
 	return str;
+}
 
+const char* MyString::c_str() const
+{
+	return str;
+}
+
+bool MyString::empty() const
+{
+	return sz == 0;
 }
 
 
--- OAiP/lab4/MyString/MyString.h
+++ OAiP/lab4/MyString/MyString.h
@@ -1,10 +1,11 @@
 #pragma once
-#pragma once
+#include <cstring>
+#include <stdexcept>
 
 #ifdef MyString_EXPORTS
-#define MyString_API __declspec(dllexport)
-#else
 #define MyString_API __declspec(dllimport)
+#else
+#define MyString_API __declspec(dllexport)
 #endif
 
 class MyString_API MyString
@@ -12,28 +13,429 @@ class MyString_API MyString
 private:
 	char* str = nullptr;
 	size_t sz = 0;
-	//size_t cap = 0;
+	size_t cap = 0;
 public:
-	MyString() {}
-	MyString(size_t size);
-	MyString(size_t size, char ch);
+	MyString();
+	MyString(size_t);
+	MyString(size_t, char);
 	MyString(const MyString&);
 	MyString(const char[]);
 	~MyString();
+	size_t size() const;
+	size_t capacity() const;
+	void reserve(size_t new_cap);
+	void resize(size_t new_size, const char ch);
+	char& operator[](size_t pos);
+	char& at(size_t pos);
+	void push_back(char ch);
+	void pop_back();
+	void clear();
+	MyString& append(size_t count, char ch);
+	char& front();
+	char& back();
+	const char* data() const;
+	const char* c_str() const;
+	bool empty() const;
 
-	void* memcpy(void* s1, const void* s2, size_t n);
-	void* memmove(void* s1, const void* s2, size_t n);
-	char* strcpy(char* dest, const char* src);
-	char* strncpy(char* dest, const char* src, size_t n);
-	char* strcat(char* dest, const char* src);
-	char* strncat(char* dest, const char* src, size_t n);
-	int memcmp(const void* s1, const void* s2, size_t n);
-	int strcmp(const char* s1, const char* s2);
-	int strcoll(const char* s1, const char* s2);
-	int strncmp(const char* s1, const char* s2, size_t n);
-	size_t strxfrm(char* s1, const char* s2, size_t n);
-	bool findChar(const char* str, char ch);
-	char* strtok(char* str, const char* delim);
+	//====================================================
+	//================== ITERATORS =======================
+	//====================================================
 
+	class MyIterator {
+	public:
+		using iterator_category = std::random_access_iterator_tag;
+		using value_type = char;
+		using difference_type = std::ptrdiff_t;
+		using pointer = char*;
+		using reference = char&;
+	private:
+		pointer ptr;
+	public:
+		MyIterator() {}
+
+		MyIterator(pointer ptr) : ptr(ptr) {}
+
+		MyIterator operator++(int) {
+			MyIterator tmp = *this;
+			++ptr;
+			return tmp;
+		}
+
+		MyIterator& operator++() {
+			++ptr;
+			return *this;
+		}
+
+		MyIterator operator--(int) {
+			MyIterator tmp = *this;
+			--ptr;
+			return tmp;
+		}
+
+		MyIterator& operator--() {
+			--ptr;
+			return *this;
+		}
+
+		bool operator==(const MyIterator& other) const {
+			return ptr == other.ptr;
+		}
+
+		bool operator!=(const MyIterator& other) const {
+			return ptr != other.ptr;
+		}
+
+		MyIterator operator+(size_t n) {
+			return MyIterator(ptr + n);
+		}
+
+		MyIterator operator-(size_t n) {
+			return MyIterator(ptr - n);
+		}
+
+		MyIterator operator+=(size_t n) {
+			ptr += n;
+			return *this;
+		}
+
+		MyIterator operator-=(size_t n) {
+			ptr -= n;
+			return *this;
+		}
+
+		reference operator*() {
+			return *ptr;
+		}
+
+		pointer operator->() {
+			return ptr;
+		}
+
+		size_t operator-(const MyIterator& other) {
+			return ptr - other.ptr;
+		}
+	};
+
+	class MyConstIterator {
+	public:
+		using iterator_category = std::random_access_iterator_tag;
+		using value_type = const char;
+		using difference_type = std::ptrdiff_t;
+		using pointer = const char*;
+		using reference = const char&;
+	private:
+		pointer ptr;
+	public:
+		MyConstIterator() {}
+
+		MyConstIterator(pointer ptr) : ptr(ptr) {}
+
+		MyConstIterator operator++(int) {
+			MyConstIterator tmp = *this;
+			++ptr;
+			return tmp;
+		}
+
+		MyConstIterator& operator++() {
+			++ptr;
+			return *this;
+		}
+
+		MyConstIterator operator--(int) {
+			MyConstIterator tmp = *this;
+			--ptr;
+			return tmp;
+		}
+
+		MyConstIterator& operator--() {
+			--ptr;
+			return *this;
+		}
+
+		bool operator==(const MyConstIterator& other) const {
+			return ptr == other.ptr;
+		}
+
+		bool operator!=(const MyConstIterator& other) const {
+			return ptr != other.ptr;
+		}
+
+		MyConstIterator operator+(size_t n) {
+			return MyConstIterator(ptr + n);
+		}
+
+		MyConstIterator operator-(size_t n) {
+			return MyConstIterator(ptr - n);
+		}
+
+		MyConstIterator operator+=(size_t n) {
+			ptr += n;
+			return *this;
+		}
+
+		MyConstIterator operator-=(size_t n) {
+			ptr -= n;
+			return *this;
+		}
+
+		reference operator*() {
+			return *ptr;
+		}
+
+		pointer operator->() {
+			return ptr;
+		}
+
+		size_t operator-(const MyConstIterator& other) {
+			return ptr - other.ptr;
+		}
+	};
+
+	using iterator = MyIterator;
+	using const_iterator = MyConstIterator;
+	using  reverse_iterator = std::reverse_iterator<MyIterator>;
+	using  const_reverse_iterator = std::reverse_iterator<MyConstIterator>;
+
+	iterator begin() {
+		return iterator(str);
+	}
+
+	iterator end() {
+		return str ? iterator(str + sz) : iterator(str);
+	}
+
+	const_iterator cbegin() const {
+		return const_iterator(str);
+	}
+
+	const_iterator cend() const {
+		return str ? const_iterator(str + sz) : const_iterator(str);
+	}
+
+	reverse_iterator rbegin() {
+		return reverse_iterator(end());
+	}
+
+	reverse_iterator rend() {
+		return reverse_iterator(begin());
+	}
+
+	const_reverse_iterator crbegin() const {
+		return const_reverse_iterator(cend());
+	}
+
+	const_reverse_iterator crend() const {
+		return const_reverse_iterator(cbegin());
+	}
+	
+	//====================================================
+	//================== END OF ITERATORS =======================
+	//====================================================
 };
+
+void* my_memcpy(void* dest, const void* src, size_t n)
+{
+	char* p1 = (char*)dest;
+	const char* p2 = (const char*)src;
+	while (n--) {
+		*p1++ = *p2++;
+	}
+	return p1;
+}
+
+void* my_memmove(void* dest, const void* src, size_t n)
+{
+	char* p1 = (char*)dest;
+	const char* p2 = (const char*)src;
+	while (n--) {
+		*p1++ = *p2++;
+	}
+	return p1;
+}
+
+char* my_strcpy(char* dest, const char* src)
+{
+	char* p1 = dest;
+	const char* p2 = src;
+	while (*p2) {
+		if (*p1 == '\0' && *p2 != '\0') {
+			throw std::exception("strcpy: dest is too small");
+		}
+		*p1++ = *p2++;
+	}
+	*p1 = '\0';
+	return p1;
+}
+
+char* my_strncpy(char* dest, const char* src, size_t n)
+{
+	size_t already_copied = 0;
+	char* p1 = dest;
+	const char* p2 = src;
+	while (n-- && *p2) {
+		if (*p1 == '\0' && already_copied < n)
+		{
+			throw std::exception("strncpy: dest is too small");
+		}
+		*p1++ = *p2++;
+		++already_copied;
+	}
+	if (!*p2) {
+		*p1 = '\0';
+		++already_copied;
+	}
+	else if (already_copied != n) // if the source string is shorter than n
+	{
+		while (already_copied < n) {
+			*p1++ = '\0';
+			++already_copied;
+		}
+	}
+	return p1;
+}
+
+char* my_strcat(char* dest, const char* src)
+{
+	char* p1 = dest;
+	while (*p1) {
+		p1++;
+	}
+	const char* p2 = src;
+	while (*p2) {
+		*p1++ = *p2++;
+	}
+	*p1 = '\0';
+	return p1;
+}
+
+char* my_strncat(char* dest, const char* src, size_t n)
+{
+	char* p1 = dest;
+	while (*p1) {
+		p1++;
+	}
+	const char* p2 = src;
+	while (n-- && *p2) {
+		*p1++ = *p2++;
+	}
+	*p1 = '\0';
+	return p1;
+}
+
+int  my_memcmp(const void* s1, const void* s2, size_t n)
+{
+	const char* p1 = (const char*)s1;
+	const char* p2 = (const char*)s2;
+	while (n--) {
+		if (*p1 != *p2) {
+			return *p1 - *p2;
+		}
+		p1++;
+		p2++;
+	}
+	return 0;
+}
+
+int  my_strcmp(const char* s1, const char* s2)
+{
+	while (*s1 && *s2) {
+		if (*s1 != *s2) {
+			return *s1 - *s2;
+		}
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
+}
+
+int  my_strcoll(const char* s1, const char* s2)
+{
+	return std::strcoll(s1, s2);
+}
+
+int  my_strncmp(const char* s1, const char* s2, size_t n)
+{
+	while (n-- && *s1 && *s2) {
+		if (*s1 != *s2) {
+			return *s1 - *s2;
+		}
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
+}
+
+size_t  my_strxfrm(char* s1, const char* s2, size_t n)
+{
+	return std::strxfrm(s1, s2, n);
+}
+
+bool  findChar(const char* str, char ch)
+{
+	while (*str) {
+		if (*str == ch) {
+			return true;
+		}
+		str++;
+	}
+	return false;
+}
+
+//char* my_strtok(char* str, const char* delim)
+//{
+//	char check1 = '\0';
+//	char first = *str;
+//	char* first_address = nullptr;
+//	char second;
+//	while (*str) {
+//		first_address = str;
+//		first = *str;
+//		if (*str && !findChar(delim, first)) {
+//			check1 = 'c';
+//			break;
+//		}
+//		str++;
+//	}
+//	if (check1 == '\0') {
+//		return nullptr;
+//	}
+//	char check2 = '\0';
+//	while (*(++first_address)) {
+//		second = *first_address;
+//		if (*first_address && findChar(delim, second)) {
+//			second = '\0';
+//			check2 = 'c';
+//			break;
+//		}
+//		first_address++;
+//	}
+//	if (check2 != '\0') {
+//		*(str) = '\0';
+//	}
+//	return str;
+//}
+
+void* my_memcet(void* dest, int ch, size_t count) {
+	char* p1 = (char*)dest;
+	unsigned char value = (unsigned char)ch;
+	while (count--) {
+		*p1++ = value;
+	}
+	return p1;
+}\
+
+//char* my_strerror(int errnum) {
+//	return std::strerror(errnum);
+//}
+
+size_t my_strlen(const char* str) {
+	size_t len = 0;
+	while (*str) {
+		len++;
+		str++;
+	}
+	return len;
+}
+
+
 
