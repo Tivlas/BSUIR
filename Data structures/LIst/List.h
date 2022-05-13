e987cdfaa3bc41da586c02b32bde0a0aa14434ba
Tivlas <tima021003tima@gmail.com>
2022-05-14 00:28:01 +0300

    Не доделал хеш-таблицу

---
+++ Data structures/LIst/List.h
@@ -0,0 +1,161 @@
+#pragma once
+#include <memory>
+template <typename T>
+struct Node
+{
+	Node(T data)
+	{
+		this->data = data;
+	}
+	T data;
+	Node* next = nullptr;
+	Node* prev = nullptr;
+};
+
+template <typename T>
+class List {
+	using pointer = Node<T>*;
+public:
+	template <typename U>
+	class BidIterator {
+		using pointer = Node<U>*;
+	private:
+		pointer ptr = nullptr;
+	public:
+		BidIterator(pointer ptr) : ptr(ptr) {}
+
+		BidIterator& operator++() {
+			ptr = ptr->next;
+			return *this;
+		}
+
+		BidIterator& operator++(int) {
+			BidIterator tmp = *this;
+			++*this;
+			return tmp;
+		}
+
+		BidIterator& operator--() {
+			ptr = ptr->prev;
+			return *this;
+		}
+
+		BidIterator& operator--(int) {
+			BidIterator tmp = *this;
+			--*this;
+			return tmp;
+		}
+
+		U& operator*() {
+			return ptr->data;
+		}
+
+		U* operator->() {
+			return std::addressof(ptr->data);
+		}
+		
+		bool operator==(const BidIterator& other) {
+			return ptr == other.ptr;
+		}
+
+		bool operator!=(const BidIterator& other) {
+			return ptr != other.ptr;
+		}
+	};
+	
+	using iterator = BidIterator<T>;
+	using const_iterator = BidIterator<const T>;
+private:
+	pointer head = nullptr;
+	pointer tail = nullptr;
+	size_t sz = 0;
+public:
+	List() {};
+
+	~List() {
+		pointer next = nullptr;
+		while (head) {
+			next = head->next;
+			delete head;
+			head = next;
+		}
+	}
+
+	void add(const T& data) {
+		pointer new_node = new Node<T>(data);
+		if (head == nullptr) {
+			head = new_node;
+			tail = new_node;
+		}
+		else {
+			tail->next = new_node;
+			new_node->prev = tail;
+			tail = new_node;
+		}
+		sz++;
+	}
+
+	void remove(const T& value) {
+		pointer cur = head;
+		while (cur) {
+			if (cur->data == value) {
+				if (cur == head) {
+					head = cur->next;
+					if (head) {
+						head->prev = nullptr;
+					}
+					else {
+						tail = nullptr;
+					}
+				}
+				else if (cur == tail) {
+					tail = cur->prev;
+					tail->next = nullptr;
+				}
+				else {
+					cur->prev->next = cur->next;
+					cur->next->prev = cur->prev;
+				}
+				delete cur;
+				sz--;
+				return;
+			}
+			cur = currcurent->next;
+		}
+	}
+
+	T* search(const T& value) {
+		pointer cur = head;
+		while (cur) {
+			if (cur->data == value) {
+				return std::addressof(cur->data);
+			}
+			cur = cur->next;
+		}
+		return nullptr;
+	}
+
+	bool empty() const {
+		return sz == 0;
+	}
+
+	void clear() {
+		pointer next = nullptr;
+		while (head) {
+			next = head->next;
+			delete head;
+			head = next;
+		}
+		sz = 0;
+	}
+
+	size_t size() const { return sz; }
+
+	iterator begin() { return iterator(head); }
+
+	iterator end() { return iterator(nullptr); }
+
+	const_iterator cbegin() const { return const_iterator(head); }
+
+	const_iterator cend() const { return const_iterator(nullptr); }
+};
--- OAiP/lab6/task2/HashTable.h
+++ OAiP/lab6/task2/HashTable.h
@@ -1,5 +1,55 @@
 #pragma once
+#include "List.h"
+class HashTable {
+private:
+	List<int>* table;
+	size_t _buckets_count; // number of buckets
+	size_t _size; // number of items in the table
 
-template <typename T>
-class HashTable { 
+	int hash(int key) {
+		return key % _buckets_count;
+	}
+public:
+	HashTable(size_t size) {
+		this->_buckets_count = size;
+		table = new List<int>[size];
+		_size = 0;
+	}
+
+	~HashTable() {
+		delete[] table;
+	}
+
+	void insert(const int item) {
+		int index = hash(item);
+		table[index].add(item);
+		++_size;
+	}
+
+	void remove(const int item) {
+		size_t index = hash(item);
+		int* delete_ptr = table[index].search(item);
+		if (delete_ptr) {
+			table[index].remove(*delete_ptr);
+			--_size;
+		}
+		--_size;
+	}
+
+	int* search(int number) {
+		size_t index = hash(number);
+		return table[index].search(number);
+	}
+
+	size_t size()const {
+		return _size;
+	}
+
+	size_t buckets_count() const {
+		return _buckets_count;
+	}
+
+	const List<int>& get_bucket(size_t index) const {
+		return table[index];
+	}
 };
