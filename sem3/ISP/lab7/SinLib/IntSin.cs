a1c32fdf7387b68489211dcd8ba967cdde4eb3f2
Tivlas <tima021003tima@gmail.com>
2022-12-10 14:43:12 +0300

    Прекрасная 7ая лаба

---
+++ sem3/ISP/lab7/SinLib/IntSin.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+
+namespace SinLibSpace
+{
+    public class IntSin
+    {
+        private object _calcLock = new object();
+        public event EventHandler<IntSinEventArgs>? SinCalculated;
+        public event EventHandler<ProgressEventArgs>? ProgressEvent;
+        private double _step = 0.000000001;
+        private Semaphore _semaphore = new(4, 10);
+        public void CalcSinIntegral()
+        {
+            //lock (_calcLock)
+            {
+                _semaphore.WaitOne();
+
+                Thread cur = Thread.CurrentThread;
+                double border = 0;
+                double sin = 0;
+
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                int percent = 0;
+                int prevPercent = -1;
+                while (border <= 1)
+                {
+                    sin += Math.Sin(border) * _step;
+                    border += _step;
+                    percent = (int)(border / 1.0 * 100);
+
+                   
+
+                    if (percent != prevPercent)
+                    {
+                        ProgressEvent?.Invoke(this, new ProgressEventArgs(percent));
+                        prevPercent = percent;
+                    }
+                }
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+                SinCalculated?.Invoke(this, new IntSinEventArgs(sin, elapsedTime, cur.ManagedThreadId,cur.Name!));
+                _semaphore.Release();
+            }
+        }
+    }
+
+    public class IntSinEventArgs : EventArgs
+    {
+        public readonly double Sin;
+        public readonly string ElapsedTime;
+        public int ThreadID;
+        public string ThreadName;
+        public IntSinEventArgs(double _sin, string _elapsedTime, int id, string name = "")
+        {
+            Sin = _sin;
+            ElapsedTime = _elapsedTime;
+            ThreadID = id;
+            ThreadName = name;
+        }
+    }
+
+    public class ProgressEventArgs : EventArgs
+    {
+        public readonly int Percent;
+        public ProgressEventArgs(int percent)
+        {
+            Percent = percent;
+        }
+    }
+}
---
+++ sem3/ISP/lab7/Task/Program.cs
@@ -0,0 +1,74 @@
+﻿using SinLibSpace;
+using System.Text;
+namespace Task
+{
+    internal class Program
+    {
+        private static object _lock = new();
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            IntSin obj = new();
+            StringBuilder progress = new();
+            int size = 10;
+            obj.SinCalculated += (_, eventArgs) =>
+            {
+                lock (_lock)
+                {
+                    int top = int.Parse(Thread.CurrentThread.Name!);
+                    Console.SetCursorPosition(0, size + top);
+                    Console.WriteLine($"Sin equals: {eventArgs.Sin:f4}. Elapsed time: {eventArgs.ElapsedTime}. Thread ID = {eventArgs.ThreadID,2}/Name = {eventArgs.ThreadName,-2} has finished it's work.\n"); 
+                }
+            };
+
+            obj.ProgressEvent += (_, eventArgs) =>
+            {
+                lock (_lock)
+                {
+                    int percent = eventArgs.Percent;
+                    int top = int.Parse(Thread.CurrentThread.Name!);
+                    StringBuilder progress = new StringBuilder("[");
+                    for (int i = 0; i < 100; i++)
+                    {
+                        if (i < percent)
+                        {
+                            progress.Append("=");
+                        }
+                        else if (i == percent)
+                        {
+                            progress.Append(">");
+                        }
+                        else
+                        {
+                            progress.Append(" ");
+                        }
+                    }
+                    progress.Append("]");
+                    progress.Append(percent.ToString() + '%');
+                    Console.SetCursorPosition(0, top);
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId,-2} with {Thread.CurrentThread.Priority,7} priority is working: {progress}"); 
+                }
+            };
+
+            Thread[] threads = new Thread[size];
+            for (int i = 0; i < size; i++)
+            {
+                ThreadStart start = obj.CalcSinIntegral;
+                start += () =>
+                {
+                    lock (_lock)
+                    {
+                        Console.SetCursorPosition(0, size * 2 + 1); 
+                    }
+                };
+                threads[i] = new Thread(start) { Name = (i + 1).ToString(), Priority = ThreadPriority.Normal };
+            }
+            threads[0].Priority = ThreadPriority.Highest;
+            threads[^1].Priority = ThreadPriority.Lowest;
+            foreach (Thread th in threads)
+            {
+                th.Start();
+            }
+        }
+    }
+}
