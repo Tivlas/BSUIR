1281b5afd2a73d98f0c6a694a7028001a54826d2
Tivlas <tima021003tima@gmail.com>
2022-12-18 13:30:28 +0300

    8 лаба

---
+++ sem3/ISP/lab8/TaskLib/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskLib
+{
+    public class Car
+    {
+        public int ID { get; set; } = 0;
+        public string Name { get; set; } = "No Name";
+        public int EngineCapacity { get; set; } = 0;
+
+        public Car(int id, string name, int engineCapacity)
+        {
+            ID = id;
+            Name = name;
+            EngineCapacity = engineCapacity;
+        }
+
+        public Car()
+        {
+        }
+    }
+}
