4e103453092f017d2cbc2224c2b3104ae5a382c7
Tivlas <tima021003tima@gmail.com>
2023-02-06 20:04:19 +0300

    Ограничил размеры окна

--- sem4/ISP/lab1/MauiCalculator/App.xaml.cs
+++ sem4/ISP/lab1/MauiCalculator/App.xaml.cs
@@ -8,4 +8,17 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var windows = base.CreateWindow(activationState);
+
+        windows.MinimumHeight = 600;
+        windows.MinimumWidth = 720;
+
+        windows.MaximumHeight = 800;
+        windows.MaximumWidth = 720;
+
+        return windows;
+    }
 }
