54be7c77fa95df034f0998684041fea4f129a1b6
Tivlas <tima021003tima@gmail.com>
2023-03-31 23:06:22 +0300

    Валидация ввода

---
+++ sem4/ISP/lab1/MauiCalculator/Lab4/Services/EntryInputValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MauiCalculator.Lab4.Services
+{
+    public static class EntryInputValidator
+    {
+        public static bool IsValid(string input)
+        {
+            string pattern = @"^[1-9]{1,7}(\.\d{0,2})?$";
+            return Regex.IsMatch(input, pattern);
+        }
+    }
+}
