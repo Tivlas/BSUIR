14d00895194bd2a6b5c7f58f94140684174d4772
Tivlas <tima021003tima@gmail.com>
2023-08-22 12:46:29 +0300

    Начал делать первую лабу.

---
+++ sem5/SPPR/Labs/WEB_153505_Vlasenko/Controllers/Home.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153505_Vlasenko.Controllers;
+public class Home : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ sem5/SPPR/Labs/WEB_153505_Vlasenko/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WEB_153505_Vlasenko.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
