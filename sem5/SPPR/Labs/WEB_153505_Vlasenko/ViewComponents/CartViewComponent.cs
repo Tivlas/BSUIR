5ab60faae5c7cc4e390f2437b4b885c65c5d167a
Tivlas <tima021003tima@gmail.com>
2023-08-23 15:18:34 +0300

    Добавил cart view component, partial view для него. (шарпы лаба 2)

---
+++ sem5/SPPR/Labs/WEB_153505_Vlasenko/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153505_Vlasenko.ViewComponents;
+
+public class Cart : ViewComponent
+{
+	public Task<IViewComponentResult> InvokeAsync()
+	{
+        return Task.FromResult<IViewComponentResult>(View());
+	}
+}
