590b74848f7e644ca6fe42031c7e5d5dca939ad8
Tivlas <tima021003tima@gmail.com>
2023-08-23 18:45:26 +0300

    Страница Product с выводом пары позиций (в виде таблицы).

---
+++ sem5/SPPR/Labs/WEB_153505_Vlasenko/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_153505_Vlasenko.Services.ClothesCategoryService;
+using WEB_153505_Vlasenko.Services.ClothesService;
+
+namespace WEB_153505_Vlasenko.Controllers;
+public class ProductController : Controller
+{
+	private readonly IClothesService _clothesService;
+	private readonly IClothesCategoryService _clothesCategoryService;
+
+	public ProductController(IClothesService clothesService, IClothesCategoryService clothesCategoryService)
+	{
+		_clothesService = clothesService;
+		_clothesCategoryService = clothesCategoryService;
+	}
+
+	public async Task<IActionResult> Index()
+	{
+		var productResponse = await _clothesService.GetClothesListAsync(null);
+		if (!productResponse.Success)
+		{
+			return NotFound(productResponse.ErrorMessage);
+		}
+		return View(productResponse.Data!.Items);
+	}
+}
