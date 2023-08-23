e3f9bcd1c883263f7880ffee22bf827fb3039243
Tivlas <tima021003tima@gmail.com>
2023-08-23 12:01:49 +0300

    Добавил select в форму. Удалил все лишнее из home/index.

--- sem5/SPPR/Labs/WEB_153505_Vlasenko/Controllers/Home.cs
+++ sem5/SPPR/Labs/WEB_153505_Vlasenko/Controllers/Home.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WEB_153505_Vlasenko.Models;
 
 namespace WEB_153505_Vlasenko.Controllers;
 public class Home : Controller
@@ -6,6 +7,15 @@ public class Home : Controller
     public IActionResult Index()
     {
         ViewData["LabMessage"] = "Лабораторная работа №2";
-        return View();
-    }
+		
+		var demoList = new List<ListDemo>
+		{
+			new ListDemo { Id = 1, Name = "Item 1" },
+			new ListDemo { Id = 2, Name = "Item 2" },
+			new ListDemo { Id = 3, Name = "Item 3" }
+		};
+
+		ViewData["DemoList"] = demoList;
+		return View();
+	}
 }
---
+++ sem5/SPPR/Labs/WEB_153505_Vlasenko/Models/ListDemo.cs
@@ -0,0 +1,7 @@
+﻿namespace WEB_153505_Vlasenko.Models;
+
+public class ListDemo
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+}
