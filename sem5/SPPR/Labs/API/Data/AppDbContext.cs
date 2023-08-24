76f409196d7187ccf6917671ddf9e020142e6521
Tivlas <tima021003tima@gmail.com>
2023-08-24 15:50:10 +0300

    Зарегал БД сервис.

---
+++ sem5/SPPR/Labs/API/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Clothes> Clothes { get; set; }	
+    public DbSet<ClothesCategory> ClothesCategory { get; set; }
+
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options)
+	{
+		Database.EnsureCreated();
+	}
+}
--- sem5/SPPR/Labs/API/Program.cs
+++ sem5/SPPR/Labs/API/Program.cs
@@ -1,3 +1,8 @@
+using System.Reflection;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+RegisterDbContext(builder);
 
 var app = builder.Build();
 
@@ -25,3 +31,12 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void RegisterDbContext(WebApplicationBuilder builder)
+{
+	var connectionString = builder.Configuration
+		   .GetConnectionString("Default");
+	string dataDirectory = AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar;
+    connectionString = string.Format(connectionString!, dataDirectory);
+	builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString).EnableSensitiveDataLogging());
+}
