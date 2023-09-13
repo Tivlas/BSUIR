3dfbd5e0a72e8da8f3900d0fa09677908d852598
Tivlas <tima021003tima@gmail.com>
2023-09-13 17:35:27 +0300

    4ая таска 1ой лабы.

---
+++ sem5/VPO/Lab-1/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task4;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		int rowCount = 200;
+		int columnCount = 5;
+
+		string html = GenerateHtmlTable(rowCount, columnCount);
+		File.WriteAllText("table.html", html);
+	}
+
+	static string GenerateHtmlTable(int rowCount, int columnCount)
+	{
+		string html = "<!DOCTYPE html>\n";
+		html += "<html>\n<head>\n";
+		html += "</head>\n<body>\n";
+		html += "<style\n>" +
+			   "table { border-collapse: collapse; width: 100%; }\n" +
+			   "th, td { padding: 5px;}\n" +
+			   "</style>\n";
+		html += "<table>\n";
+
+		for (int row = rowCount; row >= 0; row--)
+		{
+			html += "<tr>\n";
+			for (int col = 0; col < columnCount; col++)
+			{
+				int grayValue = (int)Math.Round((row / (double)rowCount) * 255);
+				string backgroundColor = $"rgb({grayValue}, {grayValue}, {grayValue})";
+
+				html += $"<td style=\"background-color: {backgroundColor};\"></td>\n";
+			}
+			html += "</tr>\n";
+		}
+
+		html += "</table>\n";
+		html += "</body>\n</html>";
+
+		return html;
+	}
+}
