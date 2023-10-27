34bc7a78ab5c75131459fe703b2a469537fca48b
Tivlas <tima021003tima@gmail.com>
2023-10-27 16:06:42 +0300

    Зарегестрировал сервис и использовал на странице объектов.

--- sem5/SPPR/Labs/BlazorWasm/Program.cs
+++ sem5/SPPR/Labs/BlazorWasm/Program.cs
@@ -1,4 +1,5 @@
 using BlazorWasm;
+using BlazorWasm.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetSection("ApiUri").Value!) });
+
+builder.Services.AddScoped<IDataService, DataService>();
 
 builder.Services.AddOidcAuthentication(options =>
 {
