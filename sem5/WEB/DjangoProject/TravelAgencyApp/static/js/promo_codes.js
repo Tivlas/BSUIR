25d54fa9129ab00255e25a4ab4088b4d114d42f6
Tivlas <tima021003tima@gmail.com>
2023-11-02 11:19:00 +0300

    Пункт 6 из условия веб-3.

---
+++ sem5/WEB/DjangoProject/TravelAgencyApp/static/js/promo_codes.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(".active-discount").each(function () {
+        var discountSpan = $(this).find(".discount");
+        var discountValue = parseInt(discountSpan.text());
+        $({ value: 0 }).animate(
+            { value: discountValue },
+            {
+                duration: 2000,
+                step: function () {
+                    discountSpan.text(Math.floor(this.value) + "%");
+                },
+                complete: function () {
+                    discountSpan
+                        .animate({ fontSize: "2em" }, 500)
+                        .animate({ color: "red" }, 500)
+                        .delay(250)
+                        .animate({ fontSize: "1em", color: "black" }, 500);
+                }
+            }
+        );
+    });
+});
