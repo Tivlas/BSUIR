5982e1ce02abc7806e897a3099e081fb6fbd15a2
Tivlas <tima021003tima@gmail.com>
2023-11-02 13:10:18 +0300

    Пункт 11 из условия веб-3.

--- sem5/WEB/DjangoProject/TravelAgencyApp/static/js/personal_account.js
+++ sem5/WEB/DjangoProject/TravelAgencyApp/static/js/personal_account.js
@@ -14,3 +14,42 @@ if (prom !== null) {
         alert("You are a minor. Parental consent is required to use this website.");
     }
 }
+
+
+
+var books = {
+    'Smith': [{ title: 'Book 1', year: 1958 }, { title: 'Book 4', year: 1970 }],
+    'Johnson': [{ title: 'Book 2', year: 1965 }, { title: 'Book 5', year: 1959 }],
+    'Williams': [{ title: 'Book 3', year: 1962 }, { title: 'Book 6', year: 1963 }]
+};
+
+function searchBooks(event) {
+    event.preventDefault(); 
+
+    var authorInput = document.getElementById('authorInput');
+    var author = authorInput.value;
+
+    var results = document.getElementById('results');
+    results.innerHTML = ''; 
+
+    var foundBooks = books[author];
+
+    
+    if (foundBooks && foundBooks.length > 0) {
+        var resultList = document.createElement('ul');
+        for (var book of foundBooks) {
+            if (book.year >= 1960) {
+                var listItem = document.createElement('li');
+                listItem.textContent = book.title;
+                resultList.appendChild(listItem);
+            }
+        }
+        results.appendChild(resultList);
+    } else {
+        results.textContent = 'No books found by this author.';
+    }
+}
+
+
+var searchForm = document.getElementById('searchForm');
+searchForm.addEventListener('submit', searchBooks);
