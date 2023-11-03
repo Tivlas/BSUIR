562e4f1b12429090d511a456e360132a90458c70
Tivlas <tima021003tima@gmail.com>
2023-11-03 12:20:00 +0300

    Пункт 10 из условия веб-3.

--- sem5/WEB/DjangoProject/TravelAgencyApp/static/js/personal_account.js
+++ sem5/WEB/DjangoProject/TravelAgencyApp/static/js/personal_account.js
@@ -24,17 +24,17 @@ var books = {
 };
 
 function searchBooks(event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     var authorInput = document.getElementById('authorInput');
     var author = authorInput.value;
 
     var results = document.getElementById('results');
-    results.innerHTML = ''; 
+    results.innerHTML = '';
 
     var foundBooks = books[author];
 
-    
+
     if (foundBooks && foundBooks.length > 0) {
         var resultList = document.createElement('ul');
         for (var book of foundBooks) {
@@ -53,3 +53,106 @@ function searchBooks(event) {
 
 var searchForm = document.getElementById('searchForm');
 searchForm.addEventListener('submit', searchBooks);
+
+
+// Классы пункт 10
+
+function withDiscount(getPriceFunc, discount) {
+    return function () {
+        return getPriceFunc.call(this) * (1 - discount / 100);
+    }
+}
+
+function Trip(destination, duration, price) {
+    this.destination = destination;
+    this.duration = duration;
+    this.price = price;
+}
+
+Trip.prototype.getDestination = function () {
+    return this.destination;
+};
+
+Trip.prototype.setDestination = function (destination) {
+    this.destination = destination;
+};
+
+Trip.prototype.getDuration = function () {
+    return this.duration;
+};
+
+Trip.prototype.setDuration = function (duration) {
+    this.duration = duration;
+};
+
+Trip.prototype.getPrice = withDiscount(function () {
+    return this.price;
+}, 20);
+
+Trip.prototype.setPrice = function (price) {
+    this.price = price;
+};
+
+function AllInclusiveTrip(destination, duration, price, hotel) {
+    Trip.call(this, destination, duration, price);
+    this.hotel = hotel;
+}
+
+AllInclusiveTrip.prototype = Object.create(Trip.prototype);
+AllInclusiveTrip.prototype.constructor = AllInclusiveTrip;
+
+AllInclusiveTrip.prototype.getHotel = function () {
+    return this.hotel;
+};
+
+AllInclusiveTrip.prototype.setHotel = function (hotel) {
+    this.hotel = hotel;
+};
+
+///////////////////////////////
+class Trip2 {
+    constructor(destination, duration, price) {
+        this.destination = destination;
+        this.duration = duration;
+        this.price = price;
+    }
+
+    get getDestination() {
+        return this.destination;
+    }
+
+    set setDestination(destination) {
+        this.destination = destination;
+    }
+
+    get getDuration() {
+        return this.duration;
+    }
+
+    set setDuration(duration) {
+        this.duration = duration;
+    }
+
+    get getPrice() {
+        return this.price;
+    }
+
+    set setPrice(price) {
+        this.price = price;
+    }
+}
+
+class AllInclusiveTrip2 extends Trip2 {
+    constructor(destination, duration, price, hotel) {
+        super(destination, duration, price);
+        this.hotel = hotel;
+    }
+
+    get getHotel() {
+        return this.hotel;
+    }
+
+    set setHotel(hotel) {
+        this.hotel = hotel;
+    }
+}
