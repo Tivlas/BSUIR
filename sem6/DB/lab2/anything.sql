ac45cb5c9d0f1c1cdba8f0168f2d56969c24ace0
Tivlas <tima021003tima@gmail.com>
2024-02-17 18:56:56 +0300

    Добавил скрипты для тестирования работы лабы 2 БД.

--- sem6/DB/lab2/anything.sql
+++ sem6/DB/lab2/anything.sql
@@ -1,1 +1,11 @@
-SELECT * from GROUPS
+SELECT *
+FROM groups;
+
+SELECT *
+FROM students_log
+ORDER BY action_date;
+
+SELECT *
+FROM students;
+
+CALL restore_students_state('09-FEB-24 11.23.11.505000000 PM');
