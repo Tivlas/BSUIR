6dee7483e98243bf67c90b6abca77196abe36592
Tivlas <tima021003tima@gmail.com>
2024-02-17 18:46:47 +0300

    Убрал внешний ключ при создании таблицы студнетов. Лаба 2 БД.

--- sem6/DB/lab2/create_table_students.sql
+++ sem6/DB/lab2/create_table_students.sql
@@ -2,6 +2,5 @@ CREATE TABLE students (
     student_id NUMBER NOT NULL,
     student_name VARCHAR2(20) NOT NULL,
     gr_id NUMBER NOT NULL,
-    CONSTRAINT student_id_pk PRIMARY KEY (student_id),
-    CONSTRAINT group_id_fk FOREIGN KEY (gr_id) REFERENCES groups (id)
+    CONSTRAINT student_id_pk PRIMARY KEY (student_id)
 )
